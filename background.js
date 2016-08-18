89a8203499d08b284b20849e604219ecfd06ab71
sungjp <gpark18@amherst.edu>
2016-08-18 15:46:13 -0400

    fetching from url

--- background.js
+++ background.js
@@ -1,10 +1,18 @@
-$(document).ready(function(){
-    setTimeout(function(){
-        var _timeArray = jQuery.makeArray($(".sorted"));
-        _timeArray.shift();
-        _timeArray.forEach(function(cur,i,arr){
-            $(cur).css("padding-bottom", "13px");
-            $(cur).after("<div style='position: absolute; transform: translateX(18px) translateY(17px);'>" + (new Date($(cur).html()).getTime()) + "</div>");
+fetch('https://code.jquery.com/jquery-3.1.0.min.js')
+    .then(r => r.text())
+    .then(r => eval(r))
+    .then(() => {
+        $(document).ready(function(){
+            setTimeout(function(){
+                var _timeArray = jQuery.makeArray($(".tripinfo-item"));
+                _timeArray.forEach(function(cur,i,arr){
+                    $(cur).find(".sorted").css("padding-bottom", "13px");
+                    $(cur).find(".sorted").after("<div style='position: absolute; transform: translateX(18px) translateY(17px);'>" + $(cur).attr("rel") + "</div>");
+
+                });
+
+            },3000);
+
         });
-    },3000);
-});
+
+    })

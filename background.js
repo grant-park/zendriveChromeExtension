7b27d74945d91b6c1c655bcf692e67d37776072c
sungjp <gpark18@amherst.edu>
2016-08-18 15:51:10 -0400

    jquery or no jquery

--- background.js
+++ background.js
@@ -1,14 +1,19 @@
-fetch('https://code.jquery.com/jquery-3.1.0.min.js')
-.then(r => r.text())
-.then(r => eval(r))
-.then(() => {
+var _exec = function(){
     $(document).ready(function(){
-        setTimeout(function(){
-            var _timeArray = jQuery.makeArray($(".tripinfo-item"));
-            _timeArray.forEach(function(cur,i,arr){
-                $(cur).find(".sorted").css("padding-bottom", "13px");
-                $(cur).find(".sorted").after("<div style='position: absolute; transform: translateX(18px) translateY(17px);'>" + $(cur).attr("rel") + "</div>");
-            });
-        },3000);
+        var _timeArray = jQuery.makeArray($(".tripinfo-item"));
+        _timeArray.forEach(function(cur,i,arr){
+            $(cur).find(".sorted").css("padding-bottom", "13px");
+            $(cur).find(".sorted").after("<div style='position: absolute; transform: translateX(18px) translateY(17px);'>" + $(cur).attr("rel") + "</div>");
+        });
     });
-})
+};
+if (!window.jQuery) {
+    fetch('https://code.jquery.com/jquery-3.1.0.min.js')
+    .then(r => r.text())
+    .then(r => eval(r))
+    .then(() => {
+        _exec();
+    });
+} else {
+    _exec();
+}

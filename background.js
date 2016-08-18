8c9ae65c576c8f454e96cef470f702ec97c70bc6
sungjp <gpark18@amherst.edu>
2016-08-18 15:47:08 -0400

    whitespace

--- background.js
+++ background.js
@@ -1,18 +1,14 @@
 fetch('https://code.jquery.com/jquery-3.1.0.min.js')
-    .then(r => r.text())
-    .then(r => eval(r))
-    .then(() => {
-        $(document).ready(function(){
-            setTimeout(function(){
-                var _timeArray = jQuery.makeArray($(".tripinfo-item"));
-                _timeArray.forEach(function(cur,i,arr){
-                    $(cur).find(".sorted").css("padding-bottom", "13px");
-                    $(cur).find(".sorted").after("<div style='position: absolute; transform: translateX(18px) translateY(17px);'>" + $(cur).attr("rel") + "</div>");
-
-                });
-
-            },3000);
-
-        });
-
-    })
+.then(r => r.text())
+.then(r => eval(r))
+.then(() => {
+    $(document).ready(function(){
+        setTimeout(function(){
+            var _timeArray = jQuery.makeArray($(".tripinfo-item"));
+            _timeArray.forEach(function(cur,i,arr){
+                $(cur).find(".sorted").css("padding-bottom", "13px");
+                $(cur).find(".sorted").after("<div style='position: absolute; transform: translateX(18px) translateY(17px);'>" + $(cur).attr("rel") + "</div>");
+            });
+        },3000);
+    });
+})
